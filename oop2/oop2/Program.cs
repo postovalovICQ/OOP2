de09f6c94fb46ae0e550e764e3a742b821510ea2
postovalovICQ <71002789+postovalovICQ@users.noreply.github.com>
2020-09-20 16:03:29 +0500

    Add files via upload

---
+++ oop2/oop2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace oop2
+{
+    class Program
+    {
+        static void NumberChannel()
+        {
+            Console.Write("Введите канал: ");
+            int channelTry;
+            bool isInt = int.TryParse(Console.ReadLine(), out channelTry);
+            if (isInt)
+            {
+                if (channelTry > 999)
+                {
+                    channelTry = 1;
+                }
+                if (channelTry < 1)
+                {
+                    channelTry = 999;
+                }
+            }
+            Channel = channelTry;
+
+            Console.WriteLine($"Текущий канал: {Channel}");
+        }
+
+        public static int Channel = 1;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Телевизор");
+            Console.WriteLine("Следующий канал: '+' ");
+            Console.WriteLine("Предыдущий канал: '-' ");
+            Console.WriteLine("Канал по номеру: '=' ");
+            bool tv = true;
+            while (tv)
+            {
+                string TV;
+                TV = Console.ReadLine();
+
+                if (TV == "-")
+
+                {
+                    if (Channel == 1)
+                    {
+                        Channel = 999;
+                    }
+                    else
+                    {
+                        Channel--;
+                    }
+                    Console.WriteLine($"Текущий канал: {Channel}");
+                }
+                else if (TV == "+")
+                {
+                    if (Channel == 999)
+                    {
+                        Channel = 1;
+                    }
+                    else
+                    {
+                        Channel++;
+                    }
+                    Console.WriteLine($"Текущий канал: {Channel}");
+                }
+                else if (TV == "=")
+                {
+                    NumberChannel();
+                }
+            }
+        }
+    }
+}
